2f8d312b5def7b76078f7e127079c89ce94447d4
NikitaGautam <nikita_0313@deerwalk.edu.np>
2015-01-19 12:08:59 +0545

    Initial commit

---
+++ src/DisplayAnimalPattern.java
@@ -0,0 +1,13 @@
+
+public class DisplayAnimalPattern {
+    public static void main (String[] args)
+    {
+        System.out.println("\t\t\t'___'");
+        System.out.println("\t\t\t(\u00A9\u00A9)");
+        System.out.println("\t/========\\/");
+        System.out.println("   / || %% ||");
+        System.out.println("  *  ||----||");
+        System.out.println("\t \u00A5¥    ¥¥");
+        System.out.println("\t \"\"    \"\"");
+    }
+}
